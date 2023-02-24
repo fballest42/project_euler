a7819b482575574cb342867d52ed94fcd049ffc9
Fernando Ballesteros <fballest@student.42madrid.com>
2023-02-24 13:33:10 +0100

    viernes, 24 de febrero de 2023, 09:38:33 CET

--- orbital/barber/barber.h
+++ orbital/barber/barber.h
@@ -15,8 +15,13 @@
 # include <time.h>
 # include <sys/time.h>
 
+/* COLOURS DEFINITION TO USE IN OUTPUTS */
+# define RED "\033[0;31m" 
+# define GREEN "\033[0;32m"
+# define BLUE "\033[0;34m"
+# define YELLOW "\033[0;33m"
+# define WHITE "\033[0;37m"
 
-# define WELLCOME "WELLCOME TO THE BARBER SHOP PROBLEM.\nBEFORE TO OPEN THE BARBER SHOP,\nWE NEED SOME INFORMATION.\n"
 typedef struct s_customer
 {
 	int				num;
@@ -29,8 +34,8 @@ typedef struct s_customer
 
 typedef struct s_data
 {
-	long			customer_num;
-	long			chairs_num;
+	int				customer_num;
+	int				chairs_num;
 	int				shaving;
 	int				time_shave;
 	int				sleep;
@@ -41,5 +46,11 @@ typedef struct s_data
 	pthread_mutex_t	*chairs;
 	pthread_mutex_t	printer;
 }				t_data;
+
+/* FUNTIONS IN BARBER_MAIN.C FILE */
+int		check_numbers(char *intro);
+char 	*valid_entry(char *question);
+int 	insert_options(t_data *data);
+void 	wellcome(t_data *data, int start_point);
 
 #endif
--- orbital/barber/barber_main.c
+++ orbital/barber/barber_main.c
@@ -1,41 +1,67 @@
 #include"barber.h"
 
-int check_numbers(char *intro, int size)
+void error_message(int num_error)
+{
+	if (num_error == 1)
+	{
+		printf(RED);
+		printf("SORRY YOUR NUMBER OF CHAIRS MUST BE BETWEEN 1 AND 9\n");
+		printf(WHITE);	
+	}
+	else if (num_error == 2)
+	{
+		printf(RED);
+		printf("SORRY YOUR NUMBER OF CUSTOMERS MUST BE BETWEEN 1 AND 999999\n");
+		printf(WHITE);	
+	}
+	else if (num_error == 3)
+	{
+		printf(RED);
+		printf("SORRY YOUR TIME MUST BE BETWEEN 50 AND 5000,\n");
+		printf("BELOW 50 IT COULD BE UNSTABLE, AND UP 5000 IS GOING TO BE TOO SLOW\n");
+		printf(WHITE);
+	}
+}
+
+int check_numbers(char *intro)
 {
 	int i = 0;
-	while (intro[i] != '\0' && intro[i] != '\n' && i < size)
+	int x = 0;
+
+	while (intro[i] != '\0')
 	{
-		if (isdigit(intro[i]))
-			i++;
+		if(isdigit(intro[i++]))
+			x++;
 		else
 			return (0);
 	}
-	return (i);
+	return (x);
 }
 
-char *valid_entry(int size, char *question)
+char *valid_entry(char *question)
 {
-	char	*chars_set;
+	char	*chars_set = NULL;
 	bool	valid = false;
+	size_t	size = 0;
 	int		ret = 0;
-	int		size_m = 0;
 	
-	chars_set = calloc(size, sizeof(char));
 	while (!valid)
 	{
 		printf("%s %s", "INSERT NUMBER OF", question);
-		fgets(chars_set, size, stdin);
-		size_m = strlen(chars_set);
-		printf("SIZE_M=%d *** CHARS_SET = %s\n", size_m, chars_set);
-		if (size >= size_m && check_numbers(chars_set, size))
+		ret = getline(&chars_set, &size, stdin);
+		chars_set[ret - 1] = '\0';
+		if (check_numbers(chars_set) != 0)
 			valid = true;
 		else
 		{
 			free(chars_set);
-			chars_set = calloc(size, sizeof(char));
+			chars_set = NULL;
+			size = 0;
 			continue;
 		}
+
 	}
+
 	return (chars_set);
 }
 
@@ -49,52 +75,58 @@ int insert_options(t_data *data)
 	{
 		if (i == 1)
 		{
-			tmp = valid_entry(3, "CHAIRS IN WAITING ROOM ? >>> ");
-			data->chairs_num = atol(tmp);
+			tmp = valid_entry("CHAIRS IN WAITING ROOM ? >>> ");
+			data->chairs_num = atoi(tmp);
 			free(tmp);
 			if (data->chairs_num > 1 && data->chairs_num < 9)
 				i = 2;
 			else
-				printf("SORRY YOUR NUMBER OF CHAIRS MUST BE BETWEEN 1 AND 9");
+				error_message(i);
 		}
 		if (i == 2)
 		{
-			tmp = valid_entry(12, "CUSTOMERS OF THE JOURNEY ? >>> ");
-			data->customer_num = atol(tmp);
+			tmp = valid_entry("CUSTOMERS OF THE JOURNEY ? >>> ");
+			data->customer_num = atoi(tmp);
 			free(tmp);
-			if (data->customer_num > 1 && data->customer_num < INT_MAX)
+			if (data->customer_num > 1 && data->customer_num < 999999)
 				i = 3;
 			else
-				printf("SORRY YOUR NUMBER OF CUSTOMERS MUST BE BETWEEN 1 AND MAX INTEGER");
+				error_message(i);
 		}
 		if (i == 3)
 		{
-			tmp = valid_entry(12, "CUSTOMERS OF THE JOURNEY ? >>> ");
-			data->customer_num = atol(tmp);
+			tmp = valid_entry("MILISECONDS FOR SETUP THE SHAVE TIME ? >>> ");
+			data->time_shave = atoi(tmp);
 			free(tmp);
-			if (data->customer_num > 1 && data->customer_num < INT_MAX)
-				i = 3;
+			if (data->time_shave >= 50 && data->time_shave <= 5000)
+				valids = true;
 			else
-				printf("SORRY YOUR NUMBER OF CUSTOMERS MUST BE BETWEEN 1 AND MAX INTEGER");
+				error_message(i);
 		}
 	}
 	return (0);
 }
 
-void wellcome(int start_point)
+void wellcome(t_data *data, int start_point)
 {
 	if (start_point == 1)
 	{
-		printf("WELLCONE TO THE PETE'S BARBER SHOP.\n");
-		printf("BEFORE TO START THE PROGRAM,\nWE NEED SOME INFORMATION TO MAKE PETE WORKS PROPERLY");
-		printf("\n*************************************************\n\n");
+		printf(YELLOW);
+		printf("*******************************************************************\n");
+		printf("*               WELLCONE TO THE PETE'S BARBER SHOP.               *\n");
+		printf("*                   BEFORE TO START THE PROGRAM,                  *\n");
+		printf("*      WE NEED SOME INFORMATION TO MAKE PETE WORKS PROPERLY       *\n");
+		printf("*******************************************************************\n\n");
+		printf(WHITE);
 	}
-	else (startpoint == 2)
+	else if (start_point == 2)
 	{
-		printf("WELLDONE.\nNOW PETE THE BARBER IS GOING TO WORK WITH YOUR CHOICE:\n");
-		printf("NUMBER OF CHAIRS IN WAITING ROOM ----------> %ld\n", data->chairs_num);
-		printf("NUMBER OF CUSTOMERS DURING THE JOURNEY ----> %ld\n",data->customer_num);
-		printf("TIME SHAVING EACH CUSTOMER %d MILISECONDS\n", data.);
+		printf(GREEN);
+		printf("WELLDONE.\nNOW PETE THE BARBER IS GOING TO WORK WITH YOUR CHOICES:\n");
+		printf("NUMBER OF CHAIRS IN WAITING ROOM ----------> %d\n", data->chairs_num);
+		printf("NUMBER OF CUSTOMERS DURING THE JOURNEY ----> %d\n",data->customer_num);
+		printf("THE SHAVING TIME FOR EACH CUSTOMER --------> %d\n", data->time_shave );
+		printf(WHITE);
 	}
 }
 
@@ -103,8 +135,9 @@ int main()
 	t_data	data;
 
 	system("clear");
-	wellcome(1);
+	wellcome(&data, 1);
 	insert_options(&data);
+	wellcome(&data, 2);
 
 	return (0);
 }
